0ede2ce4d75c49b3fe599787cb5afd6b64d1808b
Anhelina Romanchuk <angelisha3838@gmail.com>
2025-04-23 22:49:02 -0230

    Home page about and information sections done. Made sure for home page to work in any screen size properly.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import "../Styles/Home.css";
 import Header from '../Components/Header';
 
-import { MdOutlineSchool, MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdOutlineSchool, MdKeyboardDoubleArrowRight, MdWorkOutline } from "react-icons/md";
+import { IoLanguageOutline } from "react-icons/io5";
+import { FaStar, FaRegStar } from "react-icons/fa";
 
 import Banner from '../Assets/Images/Banner.png';
 import AboutPhoto from '../Assets/Images/main_photo_polaroid_nobg.png';
@@ -35,21 +37,21 @@ const Home = () => {
 
                     {/* Education Container */}
                     <div className='education'>
-                        <h2 className='ed-title'>EDUCATION</h2>
+                        <h2 className='info-title'>EDUCATION</h2>
                         <div className='ed-box'>
-                            <MdOutlineSchool className='ed-icon' />     
+                            <MdOutlineSchool className='info-icon' />     
                             <p>
                                 Vatutine school, Ukraine, Cherkasy region, Vatutine - (2010 - 2021).
                             </p>
                         </div>
                         <div className='ed-box'>
-                            <MdOutlineSchool className='ed-icon' /> 
+                            <MdOutlineSchool className='info-icon' /> 
                             <p>
                                 Cherkasy State Technological University in Software Development, Front-end course - (September 2021 - October 2022).
                             </p>
                         </div>
                         <div className='ed-box'>
-                            <MdOutlineSchool className='ed-icon' /> 
+                            <MdOutlineSchool className='info-icon' /> 
                             <p>
                                 Keyin College in Software Development - (January 2024 - Aprile 2025).
                             </p>
@@ -58,29 +60,29 @@ const Home = () => {
 
                     {/* Skills Container */}
                     <div className='skills'>
-                        <h2 className='skills-title'>SKILLS</h2>
+                        <h2 className='info-title'>SKILLS</h2>
                         <div className='skills-box'>
-                            <MdKeyboardDoubleArrowRight className='ed-icon' />
+                            <MdKeyboardDoubleArrowRight className='info-icon' />
                             <p>HTML, CSS, JavaScript, React</p>
                         </div>
                         <div className='skills-box'>
-                            <MdKeyboardDoubleArrowRight className='ed-icon' />
+                            <MdKeyboardDoubleArrowRight className='info-icon' />
                             <p>Figma</p>
                         </div>
                         <div className='skills-box'>
-                            <MdKeyboardDoubleArrowRight className='ed-icon' />
+                            <MdKeyboardDoubleArrowRight className='info-icon' />
                             <p>Digital Drawing</p>
                         </div>
                         <div className='skills-box'>
-                            <MdKeyboardDoubleArrowRight className='ed-icon' />
+                            <MdKeyboardDoubleArrowRight className='info-icon' />
                             <p>Web-testing</p>
                         </div>
                         <div className='skills-box'>
-                            <MdKeyboardDoubleArrowRight className='ed-icon' />
+                            <MdKeyboardDoubleArrowRight className='info-icon' />
                             <p>Python, Java</p>
                         </div>
                         <div className='skills-box'>
-                            <MdKeyboardDoubleArrowRight className='ed-icon' />
+                            <MdKeyboardDoubleArrowRight className='info-icon' />
                             <p>PostgresSQL/MySQL databases</p>
                         </div>
                     </div>
@@ -88,6 +90,65 @@ const Home = () => {
 
 
                 <div className='section-right'>
+                    <div className='work'>
+                        <h2 className='info-title'>WORK EXPERIENCE</h2>
+                        <div className='work-section'>
+                            
+                            <div className='work-box'>
+                                <h5>
+                                    <MdWorkOutline className='info-icon' />
+                                    Volunteering group “Bright you and brighter your future”, Ukraine
+                                </h5>
+                                <p>
+                                    Assisted with organizing HTML/CSS code cleanup and basic web testing for small businesses. Also helped plan community events, support families, and maintain electronics and documentation.
+                                </p>
+                            </div>
+                            <div className='work-box'>
+                                <h5>
+                                    <MdWorkOutline className='info-icon' />
+                                    KeyIT - (October 2022- May 2023)
+                                </h5>
+                                <p>
+                                    Worked remotely as part of a frontend team using Git and Trello for collaboration. Focused on testing websites, fixing bugs in HTML, CSS, and JavaScript, and occasionally assisting with Python backend tasks.
+                                </p>
+                            </div>
+                            <div className='work-box'>
+                                <h5>
+                                    <MdWorkOutline className='info-icon' />
+                                    AtlantiqAI - (November 2024 - Present)
+                                </h5>
+                                <p>
+                                    Currently interning with a focus on UI/UX design. Creating responsive and user-friendly interface layouts in Figma to support future frontend development.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='languages'>
+                        <h2 className='info-title'>LANGUAGES</h2>
+                        <div className='languages-box'>
+                            <IoLanguageOutline className='info-icon' />
+                            <p>Ukrainian</p>
+                            <div className='star-box'>
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                            </div>
+                        </div>
+                        <div className='languages-box'>
+                            <IoLanguageOutline className='info-icon' />
+                            <p>English</p>
+                            <div className='star-box'>
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                                <FaStar className='star-icon' />
+                                <FaRegStar className='star-icon' />
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
